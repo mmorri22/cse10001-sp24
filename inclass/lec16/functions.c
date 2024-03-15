671d15e2c4ca01c8acc000bb5f203c993a85773d
Matthew Morrison <matt.morrison@nd.edu>
2024-03-15 19:16:12 -0400

    Lecture 16 In Class

---
+++ inclass/lec16/functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Function Declarations */
+
+int main( void ){
+
+	/* Create Variables */
+	/* Note: Only create variables we are using at intermediate steps */
+
+	/* Average of two doubles */
+
+	/* Print the result */
+
+	return 0;
+}
+
+/* Function Definitions */
