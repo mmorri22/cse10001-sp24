168e48fba72fa8b80bfe5c5ee695d03ea0d8b576
Matthew Morrison <matt.morrison@nd.edu>
2024-03-16 22:13:01 -0400

    Lecture 18 In Class Update

--- inclass/lec18/setup.sh
+++ inclass/lec18/setup.sh
@@ -3,4 +3,6 @@ wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec18
 wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec18/fscanf_pbr.c
 wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec18/ptr_trace.c
 wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec18/ptr_trace_pbr.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec18/static_pbr.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec18/static_basic.c
 rm setup.sh
