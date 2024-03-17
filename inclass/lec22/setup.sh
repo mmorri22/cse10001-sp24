8ab71ef7f297bb8ff3055a6ff059f605dd76f2bb
Matthew Morrison <matt.morrison@nd.edu>
2024-03-17 16:07:58 -0400

    Lecture 22

---
+++ inclass/lec22/setup.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec22/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec22/maze_demo.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec22/maze.c
+rm setup.sh
