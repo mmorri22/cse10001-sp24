52ed996b500a98c3486633a61c3f2dadb9ee04b5
Matthew Morrison <matt.morrison@nd.edu>
2024-03-16 22:19:17 -0400

    Lecture 19 Update

---
+++ inclass/lec19/setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec19/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec19/fib.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec19/trace.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec19/trace_ptr.c
+rm setup.sh
