a0eaadf736ac41f568f953936cf70e6eb4461c5d
Matthew Morrison <matt.morrison@nd.edu>
2024-03-17 14:33:30 -0400

    Lecture 19 In Class

---
+++ inclass/lec19/setup.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec19/parallel.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec19/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec19/fact_double_sci.c
+rm setup.sh
