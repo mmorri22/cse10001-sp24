665f473c002fdad374c39442ad3595fa98911213
Matthew Morrison <matt.morrison@nd.edu>
2024-03-17 15:29:41 -0400

    Lecture 20

--- inclass/lec20/setup.sh
+++ inclass/lec20/setup.sh
@@ -1,3 +1,4 @@
 wget https://raw.githubusercontent.com/mmorri22/cse10001-s024/main/inclass/lec20/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse10001-s024/main/inclass/lec20/stat_dyn_addr.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-s024/main/inclass/lec20/void_recast.c
 rm setup.sh
