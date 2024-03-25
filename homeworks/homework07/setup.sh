6c34211b33daac6defd1a0ae915f25af1bbab7b2
Matthew Morrison <matt.morrison@nd.edu>
2024-03-25 16:41:10 -0400

    Update HW7

--- homeworks/homework07/setup.sh
+++ homeworks/homework07/setup.sh
@@ -3,4 +3,5 @@ wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/hom
 wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Prob3.c
 wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Prob4.c
 wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Prob5.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Makefile
 rm setup.sh
