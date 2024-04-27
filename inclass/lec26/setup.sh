ec8c8179a9e09f05c50dc8e9262976c56ab98529
Matthew Morrison <matt.morrison@nd.edu>
2024-04-27 12:23:45 -0400

    Lecture 26 Update

---
+++ inclass/lec26/setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec26/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec26/lec26.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec26/lec26_sol.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec26/library.txt
+rm setup.sh
