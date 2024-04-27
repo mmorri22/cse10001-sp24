394d56bbf67136453ba3ee4173d46e58f6272eed
Matthew Morrison <matt.morrison@nd.edu>
2024-04-27 11:54:11 -0400

    Lecture 25 Update

---
+++ inclass/lec25/setup.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec25/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec25/structs.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec25/mem_map.c
+rm setup.sh
