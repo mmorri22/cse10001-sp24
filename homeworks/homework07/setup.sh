9ca4933651aace25e9ac3e65cabb869b4949e9ba
Matthew Morrison <mmorri22@nd.edu>
2025-03-24 08:34:54 -0400

    Homework 07 Fix

---
+++ homeworks/homework07/setup.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Prob1.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Prob2.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Prob3.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Prob4.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Prob5.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/homeworks/homework07/Makefile
+rm setup.sh
