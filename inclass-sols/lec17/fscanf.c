5dc8c3a7deb7afff8cc501520d7a17aed8c4e999
Matthew Morrison <mmorri22@nd.edu>
2025-04-07 10:51:23 -0400

    Lecture 20 Push

--- inclass-sols/lec17/fscanf.c
+++ inclass-sols/lec17/fscanf.c
@@ -5,13 +5,20 @@ int main(){
   int first_int;
   float first_float;
   double first_double;
+  char first_array[50];
 
   /* Step 1 - Write fscanf to read in the values */
   fprintf( stdout, "Enter an integer, float, double, and a string: ");
 
+  if( fscanf( stdin, "%d %f %lf %s", &first_int, &first_float, &first_double, first_array ) ) {
+    // Secure read
+  }
+  else{
+    return 1;
+  }
 
   /* Step 2 - Write fprintf to print all the values */
-
+  fprintf( stdout, "%d %f %lf %s\n", first_int, first_float, first_double, first_array );
 
   return 0;
 }
---
+++ inclass-sols/lec20/void_recast.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h> // Dynamic Memory Allocation
+
+int main()
+{
+
+  /* Step 1a - Allocate a length of four */
+  long unsigned int array_len = 4;
+
+  /* Step 1b - Allocate a void pointer array */
+  void* void_array = malloc( array_len * sizeof(char) );
+
+  /* Step 3 - Using pointer arithmetic and ASCII hex, save values A, B, C, D */
+  *( (char *)(void_array + 0*sizeof(char)) ) = 'A';
+  *( (char *)(void_array + 1*sizeof(char)) ) = 0x42;
+  *( (char *)(void_array + 2*sizeof(char)) ) = 67;
+  *( (char *)(void_array + 3*sizeof(char)) ) = 'D';
+
+  /* Step 4 - Loop through and print addresses */
+  fprintf(stdout, "&void_array = %p,  void_array = %p\n", &void_array, void_array );
+
+  /* Step 5 - Loop through and print addresses */
+
+  long unsigned int iter;
+  for(iter = 0; iter < array_len; iter++){
+    fprintf(stdout, "void_array[%lu] addr = %p, ", iter, void_array + iter*sizeof(char) );
+
+    fprintf(stdout, "void_array[%lu] val = %c\n", iter
+        , *( (char *)(void_array + iter*sizeof(char)) ) );
+
+  }
+
+  /* Step 6 - Cast the address to int and print the result */
+  fprintf( stdout, "%p %d\n", void_array, *( (int*)void_array ) );
+
+  fprintf( stdout, "%p %f\n", void_array, *( (float*)void_array ) );
+
+  /* Step 1c - Free the void array*/
+  free(void_array);
+
+  return 0;
+}
