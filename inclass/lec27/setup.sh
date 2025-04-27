453cea5427c34570dffbf88d3a5e294283bce73c
Matthew Morrison <mmorri22@nd.edu>
2025-04-27 12:49:57 -0400

    Lecture 27 Update

---
+++ inclass/lec27/setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec27/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec27/sllist.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec27/lec27.c
+wget https://raw.githubusercontent.com/mmorri22/cse10001-sp24/main/inclass/lec27/sllist.h
+rm setup.sh
